f6bba07756fc67193d3e74f229ed02cc45e491cb
maksimVAs73 <traffikingvraj@gmail.com>
2024-11-19 18:49:40 +0900

    Add files via upload

---
+++ практика 1 тема 2/практика 1 тема 2/Program.cs
@@ -0,0 +1,46 @@
+﻿public class ExampleMethods
+{
+    
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Private method with no return value (void)
+    private void PrintHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+}
+
+/*
+public class User
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+ 
+
+
+    
+    public User(string username, string email)
+    {
+        Username = username;
+        Email = email;
+    }
+
+    
+  
+
+    public void DisplayUserInfo()
+    {
+        Console.WriteLine($"имя: {Username}");
+        Console.WriteLine($"почта: {Email}");
+      
+    }
+
+    public static void Main(string[] args)
+    {
+        User user = new User("кто то ", "что то@mail.ru");
+        user.DisplayUserInfo();
+    }
+}*/
